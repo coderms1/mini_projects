74fd245b938600e0b2dd556d8bcb9a0da8b8ccdd
simpl3man <157831131+coderms1@users.noreply.github.com>
2025-07-05 17:40:59 -0400

    Update and rename main.py to 03_mongosh.py

---
+++ 03_mongosh.py
@@ -0,0 +1,12 @@
+#03_mongosh.py
+
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017")
+db = client["test_db"]
+collection = db["messages"]
+
+collection.insert_one({"from": "Sim", "message": "Collection connected using python on vs code"})
+
+for doc in collection.find():
+  print(doc)
